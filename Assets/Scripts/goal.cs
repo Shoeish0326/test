11317a9123d9c93059e1782e6e3621b62caac279
SHOW\shoei <shoeish0326@gmail.com>
2022-05-26 13:31:45 +0900

    初めてのコミット

---
+++ Assets/Scripts/goal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class goal : MonoBehaviour
+{
+    public GameObject MainText;
+
+    // Use this for initialization
+    void Start()
+    {
+        MainText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("OnTrriger");
+            MainText.SetActive(true);
+        }
+    }
+
+
+
+}
+
+
+
